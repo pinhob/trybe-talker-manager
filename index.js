a8d7d48e979c86a4be63bb946a1c307d68c6be19
Bruno Pinho <brunopinho@outlook.com>
2021-12-08 20:49:47 -0300

    feito requisito 3. complexidade alta, refatorar depois

--- index.js
+++ index.js
@@ -8,7 +8,14 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-// const talkersRawData;
+// adaptado de: https://medium.com/@norbertofariasmedeiros/five-steps-como-gerar-um-random-token-em-javascript-1e1488a15d28
+const generateToken = () => Math.random().toString(10).substr(2);
+
+// retirado de: https://stackoverflow.com/questions/46155/whats-the-best-way-to-validate-an-email-address-in-javascript
+const validateEmail = (email) => {
+  const validEmailFormat = /\S+@\S+\.\S+/;
+  return email.match(validEmailFormat);
+};
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -39,6 +46,32 @@ app.get('/talker/:id', (request, response) => {
   }
 
   response.status(200).json(findTalkerById);
+});
+
+// req. 3:
+app.post('/login', (request, response) => {
+  const { email, password } = request.body;
+  const token = generateToken();
+  
+  if (!email || email === '') {
+    return response.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  
+  const checkEmail = validateEmail(email);
+
+  if (!checkEmail) {
+    return response.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+
+  if (!password || password === '') {
+    return response.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+
+  if (password.length < 6) {
+    return response.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' })
+  }
+
+  response.status(200).json({ token });
 });
 
 app.listen(PORT, () => {
